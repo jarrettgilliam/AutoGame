04ec294dc1fd7de2a9582e2a8e68b398ea9002d8
Jarrett Gilliam <jarrettgilliam@gmail.com>
2022-05-23 20:53:30 -0500

    Add more AutoGameService tests

--- src/Core.Tests/Services/AutoGameServiceTests.cs
+++ src/Core.Tests/Services/AutoGameServiceTests.cs
@@ -2,67 +2,243 @@
 
 using System.IO.Abstractions;
 using AutoGame.Core.Interfaces;
+using AutoGame.Core.Models;
 using AutoGame.Core.Services;
+using Xunit.Sdk;
 
 public class AutoGameServiceTests
 {
-    private Mock<ISoftwareManager> software0;
-    private Mock<ISoftwareManager> software1;
-    private IAutoGameService sut;
+    private readonly AutoGameService sut;
+    private readonly Mock<ILoggingService> loggingServiceMock = new();
+    private readonly Mock<IFileSystem> fileSystemMock = new();
+    private readonly Mock<IFile> fileMock = new();
+    private readonly Mock<ISoftwareManager> softwareMock1 = new();
+    private readonly Mock<ISoftwareManager> softwareMock2 = new();
+    private readonly Mock<ILaunchCondition> gamepadConnectedConditionMock = new();
+    private readonly Mock<ILaunchCondition> parsecConnectedConditionMock = new();
+
+    private readonly Config configMock;
 
     public AutoGameServiceTests()
     {
-        var loggingService = new Mock<ILoggingService>();
+        this.fileMock
+            .Setup(x => x.Exists(It.IsAny<string>()))
+            .Returns(true);
+        
+        this.fileSystemMock
+            .SetupGet(x => x.File)
+            .Returns(this.fileMock.Object);
+        
+        this.softwareMock1
+            .SetupGet(x => x.Key)
+            .Returns(nameof(this.softwareMock1));
 
-        this.software0 = new Mock<ISoftwareManager>();
-        this.software0.SetupGet(x => x.Key).Returns("key1");
+        this.softwareMock1
+            .Setup(x => x.FindSoftwarePathOrDefault())
+            .Returns($"/path/to/{nameof(this.softwareMock1)}");
+        
+        this.softwareMock2
+            .SetupGet(x => x.Key)
+            .Returns(nameof(this.softwareMock2));
 
-        this.software1 = new Mock<ISoftwareManager>();
-        this.software1.SetupGet(x => x.Key).Returns("key2");
-
-        var fileSystem = new Mock<IFileSystem>();
-
-        var condition1 = new Mock<ILaunchCondition>();
-        var condition2 = new Mock<ILaunchCondition>();
+        this.softwareMock2
+            .Setup(x => x.FindSoftwarePathOrDefault())
+            .Returns($"/path/to/{nameof(this.softwareMock2)}");
+        
+        this.configMock = new Config
+        {
+            EnableTraceLogging = false,
+            SoftwareKey = this.softwareMock1.Object.Key,
+            SoftwarePath = this.softwareMock1.Object.FindSoftwarePathOrDefault(),
+            LaunchWhenGamepadConnected = true,
+            LaunchWhenParsecConnected = true
+        };
 
         this.sut = new AutoGameService(
-            loggingService.Object,
-            fileSystem.Object,
+            this.loggingServiceMock.Object,
+            this.fileSystemMock.Object,
             new ISoftwareManager[]
             {
-                this.software0.Object, this.software1.Object
+                this.softwareMock1.Object,
+                this.softwareMock2.Object
             },
-            condition1.Object,
-            condition2.Object);
+            this.gamepadConnectedConditionMock.Object,
+            this.parsecConnectedConditionMock.Object);
     }
 
     [Fact]
     public void AvailableSoftware_PassedList_Matches()
     {
-        Assert.Equal(2, this.sut.AvailableSoftware.Count);
-        Assert.Equal(this.software0.Object, this.sut.AvailableSoftware[0]);
-        Assert.Equal(this.software1.Object, this.sut.AvailableSoftware[1]);
+        Assert.Collection(this.sut.AvailableSoftware,
+            s => Assert.Equal(s, this.softwareMock1.Object),
+            s => Assert.Equal(s, this.softwareMock2.Object));
     }
 
     [Fact]
-    public void CreateDefaultConfiguration_NotNull()
+    public void CreateDefaultConfiguration_Config_NotNull()
     {
         Assert.NotNull(this.sut.CreateDefaultConfiguration());
     }
 
     [Fact]
-    public void GetSoftwareByKey_ValidKey_MatchingSoftwareManager()
+    public void CreateDefaultConfiguration_Config_MatchesFirstSoftware()
     {
-        ISoftwareManager actual = this.sut.GetSoftwareByKeyOrNull(this.software0.Object.Key);
-
-        Assert.Equal(this.software0.Object, actual);
+        Config config = this.sut.CreateDefaultConfiguration();
+        
+        Assert.Equal(
+            this.softwareMock1.Object.Key,
+            config.SoftwareKey);
+        
+        Assert.Equal(
+            this.softwareMock1.Object.FindSoftwarePathOrDefault(),
+            config.SoftwarePath);
     }
 
     [Fact]
-    public void GetSoftwareByKey_InvalidKey_FirstSoftwareManager()
+    public void TryApplyConfiguration_ValidConfig_ReturnsTrue()
     {
-        ISoftwareManager actual = this.sut.GetSoftwareByKeyOrNull("badKey");
+        Assert.True(this.sut.TryApplyConfiguration(this.configMock));
+    }
 
-        Assert.Equal(this.software0.Object, actual);
+
+    [Fact]
+    public void TryApplyConfiguration_EmptySoftwarePath_ConfigErrors()
+    {
+        this.configMock.SoftwarePath = string.Empty;
+        
+        Assert.False(this.sut.TryApplyConfiguration(this.configMock));
+    }
+
+    [Fact]
+    public void TryApplyConfiguration_SoftwarePathDoesntExist_ConfigErrors()
+    {
+        this.fileMock
+            .Setup(x => x.Exists(It.IsAny<string>()))
+            .Returns(false);
+        
+        Assert.False(this.sut.TryApplyConfiguration(this.configMock));
+    }
+
+    [Fact]
+    public void TryApplyConfiguration_LaunchWhenGamepadConnectedTrue_Subscribes()
+    {
+        this.configMock.LaunchWhenGamepadConnected = true;
+        
+        this.sut.TryApplyConfiguration(this.configMock);
+        
+        this.gamepadConnectedConditionMock.VerifyAdd(x => x.ConditionMet += It.IsAny<EventHandler>(), Times.Once);
+        this.gamepadConnectedConditionMock.Verify(x => x.StartMonitoring(), Times.Once);
+    }
+
+    [Fact]
+    public void TryApplyConfiguration_LaunchWhenGamepadConnectedFalse_NoSubscribe()
+    {
+        this.configMock.LaunchWhenGamepadConnected = false;
+        
+        this.sut.TryApplyConfiguration(this.configMock);
+        
+        this.gamepadConnectedConditionMock.VerifyAdd(x => x.ConditionMet += It.IsAny<EventHandler>(), Times.Never);
+        this.gamepadConnectedConditionMock.Verify(x => x.StartMonitoring(), Times.Never);
+    }
+
+    [Fact]
+    public void TryApplyConfiguration_LaunchWhenParsecConnectedTrue_Subscribes()
+    {
+        this.configMock.LaunchWhenParsecConnected = true;
+        
+        this.sut.TryApplyConfiguration(this.configMock);
+        
+        this.parsecConnectedConditionMock.VerifyAdd(x => x.ConditionMet += It.IsAny<EventHandler>(), Times.Once);
+        this.parsecConnectedConditionMock.Verify(x => x.StartMonitoring(), Times.Once);
+    }
+
+    [Fact]
+    public void TryApplyConfiguration_LaunchWhenParsecConnectedFalse_NoSubscribe()
+    {
+        this.configMock.LaunchWhenParsecConnected = false;
+        
+        this.sut.TryApplyConfiguration(this.configMock);
+        
+        this.parsecConnectedConditionMock.VerifyAdd(x => x.ConditionMet += It.IsAny<EventHandler>(), Times.Never);
+        this.parsecConnectedConditionMock.Verify(x => x.StartMonitoring(), Times.Never);
+    }
+
+    [Fact]
+    public void TryApplyConfiguration_UnsubscribesConditionMet()
+    {
+        this.configMock.LaunchWhenGamepadConnected = true;
+        this.configMock.LaunchWhenParsecConnected = true;
+
+        this.sut.TryApplyConfiguration(this.configMock);
+        
+        this.configMock.LaunchWhenGamepadConnected = false;
+        this.configMock.LaunchWhenParsecConnected = false;
+
+        this.sut.TryApplyConfiguration(this.configMock);
+        
+        this.gamepadConnectedConditionMock.VerifyRemove(x => x.ConditionMet -= It.IsAny<EventHandler>(), Times.Once);
+        this.gamepadConnectedConditionMock.Verify(x => x.StopMonitoring(), Times.Once);
+        
+        this.parsecConnectedConditionMock.VerifyRemove(x => x.ConditionMet -= It.IsAny<EventHandler>(), Times.Once);
+        this.parsecConnectedConditionMock.Verify(x => x.StopMonitoring(), Times.Once);
+    }
+
+    [Fact]
+    public void GetSoftwareByKeyOrNull_ValidKey_ReturnsMatchingSoftware()
+    {
+        ISoftwareManager? actual = this.sut.GetSoftwareByKeyOrNull(this.softwareMock2.Object.Key);
+
+        Assert.Equal(this.softwareMock2.Object, actual);
+    }
+
+    [Fact]
+    public void GetSoftwareByKeyOrNull_InvalidKey_ReturnsFirstSoftware()
+    {
+        ISoftwareManager? actual = this.sut.GetSoftwareByKeyOrNull("badKey");
+
+        Assert.Equal(this.softwareMock1.Object, actual);
+    }
+
+
+    [Fact]
+    public void OnLaunchConditionMet_SoftwareNotRunning_Starts()
+    {
+        this.sut.TryApplyConfiguration(this.configMock);
+        
+        this.gamepadConnectedConditionMock
+            .Raise(x => x.ConditionMet += null, EventArgs.Empty);
+        
+        this.softwareMock1.Verify(
+            x => x.Start(this.configMock.SoftwarePath!), Times.Once);
+    }
+
+    [Fact]
+    public void OnLaunchConditionMet_SoftwareRunning_DoesntStart()
+    {
+        this.softwareMock1.SetupGet(x => x.IsRunning).Returns(true);
+        
+        this.sut.TryApplyConfiguration(this.configMock);
+        
+        this.softwareMock1.Verify(
+            x => x.Start(It.IsAny<string>()), Times.Never);
+    }
+
+    // [Fact]
+    // public void OnLaunchConditionMet_BadSoftwareKey_DoesntStart()
+    // {
+    //     throw new NotEmptyException();
+    // }
+
+    [Fact]
+    public void Dispose_Works()
+    {
+        this.sut.TryApplyConfiguration(this.configMock);
+        this.sut.Dispose();
+        
+        this.gamepadConnectedConditionMock.VerifyRemove(x => x.ConditionMet -= It.IsAny<EventHandler>(), Times.Once);
+        this.gamepadConnectedConditionMock.Verify(x => x.StopMonitoring(), Times.Once);
+        
+        this.parsecConnectedConditionMock.VerifyRemove(x => x.ConditionMet -= It.IsAny<EventHandler>(), Times.Once);
+        this.parsecConnectedConditionMock.Verify(x => x.StopMonitoring(), Times.Once);
     }
 }
