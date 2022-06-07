13b029424198cb9dbb823f011d4076e81bf33115
Jarrett Gilliam <jarrettgilliam@gmail.com>
2022-06-06 19:31:42 -0500

    Don't create a window when detecting controllers using OpenTK

---
+++ src/Infrastructure/Services/GameControllerService.cs
@@ -0,0 +1,47 @@
+﻿namespace AutoGame.Infrastructure.Services;
+
+using System;
+using System.Linq;
+using AutoGame.Infrastructure.Interfaces;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+internal sealed class GameControllerService : IGameControllerService, IDisposable
+{
+    private const int MAX_JOYSTICKS = 16;
+
+    public GameControllerService() =>
+        Joysticks.JoystickCallback += this.JoystickCallback;
+
+    public void Dispose() =>
+        Joysticks.JoystickCallback -= this.JoystickCallback;
+
+    public event EventHandler? GameControllerAdded;
+
+    public bool HasAnyGameControllers => Enumerable.Range(0, MAX_JOYSTICKS).Any(GLFW.JoystickPresent);
+
+    private void JoystickCallback(int joystick, ConnectedState state)
+    {
+        if (state is ConnectedState.Connected)
+        {
+            this.GameControllerAdded?.Invoke(this, EventArgs.Empty);
+        }
+    }
+    
+    /// <summary>
+    /// Copied from <see cref="OpenTK.Windowing.Desktop.Joysticks"/> 
+    /// </summary>
+    private static class Joysticks
+    {
+        private static readonly GLFWCallbacks.JoystickCallback? joystickCallback;
+
+        public static event GLFWCallbacks.JoystickCallback? JoystickCallback;
+
+        static Joysticks()
+        {
+            GLFWProvider.EnsureInitialized();
+            joystickCallback = (id, state) => JoystickCallback?.Invoke(id, state);
+            GLFW.SetJoystickCallback(joystickCallback);
+        }
+    }
+}
