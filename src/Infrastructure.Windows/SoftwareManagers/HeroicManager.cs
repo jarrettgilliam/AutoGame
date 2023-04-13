905c7d83a327a91fe1c0ab8ff02edbc697000a9d
Gabriel Forti Zonaro <zonaro@outlook.com>
2023-04-13 02:47:34 -0300

    Added Heroic Games Launcher

---
+++ src/Infrastructure.Windows/SoftwareManagers/HeroicManager.cs
@@ -0,0 +1,41 @@
+﻿namespace AutoGame.Infrastructure.Windows.SoftwareManagers;
+
+using System;
+using System.IO;
+using System.IO.Abstractions;
+using AutoGame.Core.Interfaces;
+using AutoGame.Infrastructure.Windows.Interfaces;
+using AutoGame.Infrastructure.Windows.Services;
+using Serilog;
+
+internal sealed class HeroicManager : ISoftwareManager
+{
+    public HeroicManager(
+        IUser32Service user32Service,
+        IProcessService processService
+       )
+    {
+        this.User32Service = user32Service;
+        this.ProcessService = processService;
+
+    }
+
+
+    private IUser32Service User32Service { get; }
+    private IProcessService ProcessService { get; }
+
+
+    public string Key => "Heroic";
+
+    public string Description => "Heroic Fullscreen";
+
+    public string DefaultArguments => "--fullscreen";
+     
+    public bool IsRunning(string softwarePath) => this.User32Service.FindWindow("ApplicationFrameWindow", "Heroic") != IntPtr.Zero;
+
+    public void Start(string softwarePath, string? softwareArguments)=>    
+        ProcessService.Start(softwarePath, softwareArguments).Dispose();        
+    
+
+    public string FindSoftwarePathOrDefault() => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Programs\heroic\Heroic.exe");
+}
