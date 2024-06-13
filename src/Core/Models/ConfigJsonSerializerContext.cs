4dd4a1b95412ed5f4789488f8d4bfd622a98f367
Jarrett Gilliam <jarrettgilliam@gmail.com>
2024-06-12 20:06:54 -0500

    Upgrade to dotnet 8

---
+++ src/Core/Models/ConfigJsonSerializerContext.cs
@@ -0,0 +1,8 @@
+﻿namespace AutoGame.Core.Models;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+[JsonSourceGenerationOptions(WriteIndented = true, ReadCommentHandling = JsonCommentHandling.Skip)]
+[JsonSerializable(typeof(Config))]
+internal partial class ConfigJsonSerializerContext : JsonSerializerContext;
