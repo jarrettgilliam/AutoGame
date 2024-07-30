f62d4b3aa119931901a0649731c8d03b56617fdc
Kaizonaro <zonaro@outlook.com>
2024-07-30 01:02:17 -0300

    Add One Game Launcher Pro
     - Add One Game Launcher Pro
     - Deprecate One Game Launcher

--- src/Infrastructure.Windows/SoftwareManagers/OneGameLauncherProManager.cs
+++ src/Infrastructure.Windows/SoftwareManagers/OneGameLauncherProManager.cs
@@ -5,9 +5,9 @@ using System.IO;
 using AutoGame.Core.Interfaces;
 using AutoGame.Infrastructure.Windows.Interfaces;
 
-internal sealed class OneGameLauncherManager : ISoftwareManager
+internal sealed class OneGameLauncherProManager : ISoftwareManager
 {
-    public OneGameLauncherManager(
+    public OneGameLauncherProManager(
         IUser32Service user32Service,
         IProcessService processService,
         IWindowService windowService)
@@ -21,13 +21,13 @@ internal sealed class OneGameLauncherManager : ISoftwareManager
     private IProcessService ProcessService { get; }
     private IWindowService WindowService { get; }
 
-    public string Key => "OneGameLauncher";
+    public string Key => "OneGameLauncherPro";
 
-    public string Description => "One Game Launcher";
+    public string Description => "One Game Launcher Pro";
 
-    public string DefaultArguments => "shell:appsfolder\\62269AlexShats.OneGameLauncherBeta_gghb1w55myjr2!App";
+    public string DefaultArguments => "shell:appsfolder\\62269AlexShats.OneGameLauncher_gghb1w55myjr2!App";
 
-    private IntPtr Window => this.User32Service.FindWindow("ApplicationFrameWindow", "One Game Launcher (Free) [DEPRECATED]");
+    private IntPtr Window => this.User32Service.FindWindow("ApplicationFrameWindow", "One Game Launcher (Pro)");
 
     public bool IsRunning(string softwarePath) => this.Window == this.User32Service.GetForegroundWindow();
 
